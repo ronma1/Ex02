5dfec01a43ee8c4338a34aec83b87fd5b255c690
daniel4x <daniel4x@gmail.com>
2016-12-25 23:12:10 +0200

    admin panel modifications

--- WebSite/scripts/adminStuff.js
+++ WebSite/scripts/adminStuff.js
@@ -1,3 +1,39 @@
+var lastClicked;
+
+var addParam = (Param) =>{
+  let tbody = $('#param-rows');
+  var ref = firebase.database().ref(Param + '/');
+  ref.on("value", function(snapshot) {
+    let set = snapshot.val();
+    let location;
+      for(let val in set){
+        if(Param == 'users'){
+          location = set[val]['LatestLoc'].split(' ');
+        }else{
+          location = set[val].split(' ');
+        }
+        let lat = location[0];
+        let lng = location[1];
+        var tr = 	'<tr>'+
+                  '<td>' + val + '</td>' +
+                  '<td>' + lat + '</td>' +
+                  '<td>' + lng + '</td>' +
+                  '</tr>';
+        tbody.append(tr);
+      }
+  $('#params').append(tbody);
+    }, function (error) {
+    console.log("Error: " + error.code);
+  });
+}
+
+var deleteTabaleContent = ()=>{
+  let tablereset = document.getElementById('params');
+  let size = tablereset.rows.length;
+  for (var i = size - 1; i > 0; i--) {
+    tablereset.deleteRow(i);
+  }
+}
 
 // Admin panel buttons
 window.onload = ()=>{
@@ -6,109 +42,35 @@ window.onload = ()=>{
   document.getElementById('getMap').addEventListener('click',()=>{
     window.location.href = './adminMap.html';
   });
-
   // On BLE button click
   document.getElementById('BLE').addEventListener('click',()=>{
+    lastClicked = 'BLE';
     document.getElementsByClassName('tool')[0].innerText = 'BLE Mac Address'
     $(function() {
-      $.fn.addParam = function(Param){
-        var ref = firebase.database().ref(Param + '/');
-        ref.on("value", function(snapshot) {
-          let bles = snapshot.val();
-            for(let ble in bles){
-              let location = bles[ble].split(' ');
-              let lat = location[0];
-              let lng = location[1];
-              var tr = 	'<tr>'+
-                        '<td>' + ble + '</td>' +
-                        '<td>' + lat + '</td>' +
-                        '<td>' + lng + '</td>' +
-                        '</tr>';
-              tbody.append(tr);
-            }
-            $('#params').append(tbody);
-              }, function (error) {
-              console.log("Error: " + error.code);
-            });
-      }
-      var tbody = $('#param-rows');
-      let tablereset = document.getElementById('params');
-      let size = tablereset.rows.length;
-      for (var i = size - 1; i > 0; i--) {
-        tablereset.deleteRow(i);
-      }
-      tbody.addParam('BLE');
+      deleteTabaleContent();
+      addParam('BLE');
     });
   });
 
   // On QR button click
   document.getElementById('QR').addEventListener('click',()=>{
+    lastClicked = 'QR';
     document.getElementsByClassName('tool')[0].innerText = 'QR number'
     $(function() {
-      $.fn.addParam = function(Param){
-        var ref = firebase.database().ref(Param + '/');
-        ref.on("value", function(snapshot) {
-          let qrs = snapshot.val();
-            for(let qr in qrs){
-              let location = qrs[qr].split(' ');
-              let lat = location[0];
-              let lng = location[1];
-              var tr = 	'<tr>'+
-                        '<td>' + qr + '</td>' +
-                        '<td>' + lat + '</td>' +
-                        '<td>' + lng + '</td>' +
-                        '</tr>';
-              tbody.append(tr);
-            }
-            $('#params').append(tbody);
-              }, function (error) {
-              console.log("Error: " + error.code);
-            });
-      }
-
-      var tbody = $('#param-rows');
-      let tablereset = document.getElementById('params');
-      let size = tablereset.rows.length;
-      for (var i = size - 1; i > 0; i--) {
-        tablereset.deleteRow(i);
-      }
-      tbody.addParam('QR');
+      deleteTabaleContent();
+      addParam('QR');
     });
   });
 
   // On users click
   // On QR button click
   document.getElementById('USERS').addEventListener('click',()=>{
+    lastClicked = 'users';
     document.getElementsByClassName('tool')[0].innerText = 'USER uid'
     $(function() {
-      $.fn.addParam = function(Param){
-        var ref = firebase.database().ref(Param + '/');
-        ref.on("value", function(snapshot) {
-          let qrs = snapshot.val();
-            for(let qr in qrs){
-              let location = qrs[qr]['LatestLoc'].split(' ');
-              let lat = location[0];
-              let lng = location[1];
-              var tr = 	'<tr>'+
-                        '<td>' + qr + '</td>' +
-                        '<td>' + lat + '</td>' +
-                        '<td>' + lng + '</td>' +
-                        '</tr>';
-              tbody.append(tr);
-            }
-            $('#params').append(tbody);
-              }, function (error) {
-              console.log("Error: " + error.code);
-            });
-      }
-
-      var tbody = $('#param-rows');
-      let tablereset = document.getElementById('params');
-      let size = tablereset.rows.length;
-      for (var i = size - 1; i > 0; i--) {
-        tablereset.deleteRow(i);
-      }
-      tbody.addParam('users');
+      deleteTabaleContent();
+      addParam('users');
     });
   });
+
 }
